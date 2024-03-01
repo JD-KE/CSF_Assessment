e61ad7c8f6f7bb81c381d2744660135ab3759f7a
JD-KE <60773497+JD-KE@users.noreply.github.com>
2024-03-01 10:32:25 +0800

    To start task 3

--- ecommerce/client/src/app/cart.store.ts
+++ ecommerce/client/src/app/cart.store.ts
@@ -1,5 +1,43 @@
 
 // TODO Task 2
+
+import { Injectable } from "@angular/core";
+import { Cart, LineItem } from "./models";
+import { ComponentStore } from "@ngrx/component-store";
+
+const INIT_STATE: Cart = {
+    lineItems:[]
+}
+
 // Use the following class to implement your store
-export class CartStore {
+@Injectable()
+export class CartStore extends ComponentStore<Cart> {
+
+    constructor() {
+        super(INIT_STATE)
+    }
+
+    readonly addToCart = this.updater<LineItem>(
+        (cart:Cart, item:LineItem) => {
+            const newCart:Cart = {
+                lineItems:[...cart.lineItems, item]
+            }
+            return newCart
+        }
+    )
+
+    readonly getCart = this.select(
+        (cart:Cart) => cart.lineItems
+    )
+
+    readonly getItemNoInCart = this.select(
+        (cart:Cart) => {
+            // console.info(cart)
+            // const noDupeSet = new Set(cart.lineItems)
+            // console.info(noDupeSet)
+            // const noDupe:LineItem[] = Array.from(noDupeSet)
+            // return noDupe.length
+            return cart.lineItems.length
+        }
+    )
 }
